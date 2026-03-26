d9efacc1bd2c7a00a71ca20ddf8c67a84167d3bd
Ali-Hajipour <alihajipour.it@gmail.com>
2026-03-26 12:20:52 +0100

    add _parse_level , _parse_timestamp

--- parsers/json_parser.py
+++ parsers/json_parser.py
@@ -15,3 +15,33 @@ LEVEL_MAP = {
     "critical" : "CRITICAL",
     "fatal" : "CRITICAL"
 }
+
+class JsonParser(BaseParser):
+    def parse_line(self , line : str) -> LogEntry | None:
+        line = line.strip()
+
+        if not line :
+            return None
+
+        data = json.loads(line)
+
+        if not isinstance(data , dict):
+            return None
+
+    def _parse_timestamp(self , data) -> datetime | None:
+        raw_timestamp =  data.get("timestamp") or data.get("time") or data.get("ts")
+        if not raw_timestamp:
+            return None
+        try:
+            return datetime.fromisoformat(str(raw_timestamp).strip())
+        except (ValueError , TypeError) :
+            return None
+
+
+    def _parse_level(self , data :str) -> LogEntry | None:
+        raw_level = data.get("level") or data.get("lvl") or data.get("severity")
+        if not raw_level :
+            return None
+        try:
+            return LEVEL_MAP.get(str(raw_level).strip().lower() , str(raw_level).strip().upper())
+
