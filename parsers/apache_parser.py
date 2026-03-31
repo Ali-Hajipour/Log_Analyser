2e8950d93d3d9e91af874497bec2cdc10cb42069
Ali-Hajipour <alihajipour.it@gmail.com>
2026-03-31 12:33:37 +0200

    add APACHE_PATTERN add TIMESTAMP_FORMAT

--- parsers/apache_parser.py
+++ parsers/apache_parser.py
@@ -1,3 +1,17 @@
 import re
 from datetime import datetime
 from .base import BaseParser, LogEntry
+
+APACHE_PATTERN = re.compile(
+    r'(\S+)'         
+    r' \S+'          
+    r' (\S+)'        
+    r' \[([^\]]+)\]' 
+    r' "(\S+)'   
+    r' (\S+)'    
+    r' (\S+)"'         
+    r' (\d{3})'        
+    r' (\d+|-)'
+)
+
+TIMESTAMP_FORMAT = "%d/%b/%Y:%H:%M:%S %z"
