b8160d7f1bf7349355d432f439a0753c1fe004ab
Ali-Hajipour <alihajipour.it@gmail.com>
2026-03-31 12:49:20 +0200

    add Apache_parser class add json_parser()

--- parsers/apache_parser.py
+++ parsers/apache_parser.py
@@ -1,5 +1,7 @@
 import re
 from datetime import datetime
+from re import match
+
 from .base import BaseParser, LogEntry
 
 APACHE_PATTERN = re.compile(
@@ -15,3 +17,13 @@ APACHE_PATTERN = re.compile(
 )
 
 TIMESTAMP_FORMAT = "%d/%b/%Y:%H:%M:%S %z"
+
+class ApacheParser(BaseParser):
+    def json_parser(self , line : str) -> LogEntry | None:
+        line =line.strip()
+        if not line :
+            return None
+        match = APACHE_PATTERN.match(line)
+
+        if not match :
+            return None
