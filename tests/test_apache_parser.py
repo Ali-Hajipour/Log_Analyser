3237b0847a2c312137a34b932cfa3366e56b3c81
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-02 13:55:12 +0200

    add class TestApacheParserValidInput add test_valid_line_returns_log_entry()

--- tests/test_apache_parser.py
+++ tests/test_apache_parser.py
@@ -10,3 +10,10 @@ NO_USER    = '192.168.1.1 - - [15/Jan/2024:10:23:45 +0000] "GET /index HTTP/1.1"
 @pytest.fixture
 def parser():
     return ApacheParser()
+
+
+class TestApacheParserValidInput:
+
+    def test_valid_line_returns_log_entry(self, parser):
+        entry = parser.parse_line(VALID_LINE)
+        assert isinstance(entry, LogEntry)
