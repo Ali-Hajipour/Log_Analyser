691bc17cca467e97b86c4aa877b4d67e3caf618c
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-02 13:42:32 +0200

    add Constants

--- tests/test_apache_parser.py
+++ tests/test_apache_parser.py
@@ -3,3 +3,6 @@ from datetime import datetime, timezone, timedelta
 from parsers.apache_parser import ApacheParser
 from parsers.base import LogEntry
 
+VALID_LINE = '192.168.1.1 - frank [15/Jan/2024:10:23:45 +0000] "GET /login HTTP/1.1" 404 512'
+NO_SIZE    = '192.168.1.1 - - [15/Jan/2024:10:23:45 +0000] "HEAD / HTTP/1.1" 200 -'
+NO_USER    = '192.168.1.1 - - [15/Jan/2024:10:23:45 +0000] "GET /index HTTP/1.1" 200 1024'
