43432e8dfbe98574869bfadbc60287c202fd6511
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-03 09:45:24 +0200

    test:  test_500_maps_to_warn() add:  test_500_maps_to_warn()

--- tests/test_apache_parser.py
+++ tests/test_apache_parser.py
@@ -85,3 +85,7 @@ class TestStatusToLevel:
     def test_403_maps_to_warn(self,parser):
         line = '192.168.1.1 - - [15/Jan/2024:10:23:45 +0000] "GET / HTTP/1.1" 403 512'
         assert parser.parse_line(line).level == "WARN"
+
+    def test_500_maps_to_error(self,parser):
+        line = '192.168.1.1 - - [15/Jan/2024:10:23:45 +0000] "GET / HTTP/1.1" 500 512'
+        assert parser.parse_line(line).level == "ERROR"
