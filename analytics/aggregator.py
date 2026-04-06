69e0510995bebc99fb561c054ad3a82c952aaab4
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-06 10:45:42 +0200

    add : analayse () add : main vars add : main loop

--- analytics/aggregator.py
+++ analytics/aggregator.py
@@ -2,3 +2,23 @@ from collections import Counter
 from typing import Iterator
 from statistics import mean, stdev
 from parsers.base import LogEntry
+
+def analyse(entries : Iterator[LogEntry]) -> dict :
+
+    total = 0
+    error_count = 0
+    level_count = Counter()
+    errors_by_hour : Counter()
+    source_counts = Counter()
+    ip_counts = Counter ()
+
+    for entry in entries :
+        total += 1
+
+        level = entry.level or "Uknown"
+        level_count[level] += 1
+
+        if level in(["ERROR" , "CRITICAL"]):
+            error_count += 1
+            if entry.timestamp:
+                errors_by_hour[entry.timestamp.hour] += 1
