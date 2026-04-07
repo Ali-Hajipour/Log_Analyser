94bcadcef7739fbd3d15d257c5ff24cb048d2d92
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-07 17:18:47 +0200

    add : make_entry()

--- parsers/test_aggregator.py
+++ parsers/test_aggregator.py
@@ -3,3 +3,19 @@ from datetime import datetime
 from collections import Counter
 from parsers.base import LogEntry
 from analytics.aggregator import analyse , detect_spikes
+
+def make_entry(
+        level = "INFO",
+        source = "api",
+        timestamp = None,
+        ip = None,
+        message = "test message"
+):
+    return LogEntry(
+        timestamp = timestamp,
+        level=level,
+        message=message,
+        source=source,
+        raw= "raw line",
+        extra = {"ip" : ip} if ip else {}
+    )
