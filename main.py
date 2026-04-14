2ec770cbd0bfaa1861635d34d09e6efb03f1efa1
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-14 20:24:22 +0200

    add : constant Parser Added

--- main.py
+++ main.py
@@ -3,3 +3,10 @@ import sys
 from parsers.json_parser import JSONParser
 from parsers.syslog_parser import SyslogParser
 from parsers.apache_parser import ApacheParser
+
+
+PARSERS = {
+    "json" : JSONParser,
+    "apache" : ApacheParser,
+    "syslog" : SyslogParser
+}
