86c728f6988b8b1aa69104f0b162f621c785d8f3
Ali-Hajipour <alihajipour.it@gmail.com>
2026-04-18 21:41:23 +0200

    add : parsers import

--- dashboard/app.py
+++ dashboard/app.py
@@ -4,3 +4,8 @@ import sys
 import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+from parsers.json_parser import JSONParser
+from parsers.apache_parser import ApacheParser
+from parsers.syslog_parser import SyslogParser
+from analytics.aggregator import analyse
